6592a8b0e7eb2fdc6ab894f66c645f1f9458af4f
pke1029 <pangkhangee@gmail.com>
2026-09-10 12:34:18 +0800

    Update requirements and modify import statements in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 
 # Set the title and favicon that appear in the Browser's tab bar.
